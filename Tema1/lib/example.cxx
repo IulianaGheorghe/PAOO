e709331f8ae5aaf72f8174dfae80c3cae01a4819
IulianaGheorghe <iuliana.gheorghe@student.upt.ro>
2023-10-29 16:03:31 +0200

    Implement copy constructor

--- Tema1/lib/example.cxx
+++ Tema1/lib/example.cxx
@@ -8,3 +8,12 @@ Person::Person(char* personName, int personAge, float personHeight) {
     this->height = personHeight;
     std::cout << "I was born! I'm " << this->name << std::endl;
 }
+
+Person::Person(const Person& p1) {
+    this->age = p1.age;
+    this->height = p1.height;
+    int nameLength = strlen(p1.name);
+    this->name = new char[nameLength];
+    memcpy(this->name, p1.name, nameLength);
+    std::cout << "I was cloned! I'm also " << this->name << std::endl;
+}
